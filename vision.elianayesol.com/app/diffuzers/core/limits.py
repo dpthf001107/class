b593581310398b8e0489cebf8ae0bcf1d9a1164d
dpthf001107 <dpthf001107@gmail.com>
2025-12-31 14:30:41 +0900

    251231 vision 서브모듈화 후 class에서 1차시도

--- vision.elianayesol.com/app/diffuzers/core/limits.py
+++
@@ -1,7 +0,0 @@
-import asyncio
-from app.diffuzers.core.config import MAX_CONCURRENCY
-
-_semaphore = asyncio.Semaphore(MAX_CONCURRENCY)
-
-def get_semaphore() -> asyncio.Semaphore:
-    return _semaphore
